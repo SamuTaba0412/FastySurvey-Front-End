76dd3c094c2d400e534fa098c598fd373b69b194
SamuTaba0412 <samutabares09022005@gmail.com>
2025-09-29 15:07:06 -0500

    users solved problem

--- src/views/roles/ListRoles.jsx
+++ src/views/roles/ListRoles.jsx
@@ -168,7 +168,6 @@ const ListRoles = () => {
                                     }}
                                 />
                             )}
-                            )}
                         />
                     </Box>
                 </CardContent>
